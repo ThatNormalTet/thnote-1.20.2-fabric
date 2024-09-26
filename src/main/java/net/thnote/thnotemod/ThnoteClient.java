909104f8ee04f86755847f986ac7d67b6a4a4cc4
ThatNormalTet <cetoprca@gmail.com>
2024-09-26 20:03:17 +0200

    Initial commit

---
+++ src/main/java/net/thnote/thnotemod/ThnoteClient.java
@@ -0,0 +1,10 @@
+package net.thnote.thnotemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThnoteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/thnote/thnotemod/ThnoteDataGenerator.java
@@ -0,0 +1,11 @@
+package net.thnote.thnotemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThnoteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
